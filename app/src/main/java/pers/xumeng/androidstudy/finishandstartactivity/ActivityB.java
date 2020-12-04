18787ff32d5042305513461ba5fd7d4feed1fe03
XuMeng-0 <xumeng_135@qq.com>
2020-12-04 11:26:17 +0800

    refactor: 将 AcitivityA ActivityB 重命名为 AcitivityB ActivityC

--- app/src/main/java/pers/xumeng/androidstudy/finishandstartactivity/ActivityB.java
+++ app/src/main/java/pers/xumeng/androidstudy/finishandstartactivity/ActivityB.java
@@ -1,11 +1,11 @@
 package pers.xumeng.androidstudy.finishandstartactivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
 import pers.xumeng.androidstudy.R;
 import pers.xumeng.androidstudy.util.LogUtil;
@@ -14,33 +14,39 @@ public class ActivityB extends AppCompatActivity {
 
   private static final String CLASS_NAME = ActivityB.class.getSimpleName();
 
-  private Button btnFinish;
 
   @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    LogUtil.e(CLASS_NAME, "===== onCreate =====");
+    LogUtil.e(CLASS_NAME, "==== onCreate =====");
     setContentView(R.layout.finish_start_activity_b);
-    btnFinish = findViewById(R.id.finish_start_btn_finish);
-    btnFinish.setOnClickListener(listener);
+
+    Button btnFinishFirst = findViewById(R.id.finish_start_btn_finish_first);
+    btnFinishFirst.setOnClickListener(listener);
+    Button btnFinishLast = findViewById(R.id.finish_start_btn_finish_last);
+    btnFinishLast.setOnClickListener(listener);
+    Button btnDelayFinishFirst = findViewById(R.id.finish_start_btn_delay_finish_first);
+    btnDelayFinishFirst.setOnClickListener(listener);
+    Button btnDelayFinishLast = findViewById(R.id.finish_start_btn_delay_finish_last);
+    btnDelayFinishLast.setOnClickListener(listener);
   }
 
   @Override
   protected void onStart() {
     super.onStart();
-    LogUtil.e(CLASS_NAME, "===== onStart =====");
+    LogUtil.e(CLASS_NAME, "==== onStart =====");
   }
 
   @Override
   protected void onRestart() {
     super.onRestart();
-    LogUtil.e(CLASS_NAME, "===== onRestart =====");
+    LogUtil.e(CLASS_NAME, "==== onRestart =====");
   }
 
   @Override
   protected void onResume() {
     super.onResume();
-    LogUtil.e(CLASS_NAME, "===== onResume =====");
+    LogUtil.e(CLASS_NAME, "==== onResume =====");
   }
 
   @Override
@@ -61,10 +67,44 @@ public class ActivityB extends AppCompatActivity {
     LogUtil.e(CLASS_NAME, "===== onDestroy =====");
   }
 
+  private void executeTimeConsumingTask() {
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
   private View.OnClickListener listener = new View.OnClickListener() {
     @Override
     public void onClick(View view) {
-      finish();
+      switch (view.getId()) {
+        case R.id.finish_start_btn_finish_first: {
+          finish();
+          startActivity(new Intent(ActivityB.this, ActivityC.class));
+        }
+        break;
+        case R.id.finish_start_btn_finish_last: {
+          startActivity(new Intent(ActivityB.this, ActivityC.class));
+          finish();
+        }
+        break;
+        case R.id.finish_start_btn_delay_finish_first: {
+          finish();
+          executeTimeConsumingTask();
+          startActivity(new Intent(ActivityB.this, ActivityC.class));
+        }
+        break;
+        case R.id.finish_start_btn_delay_finish_last: {
+          startActivity(new Intent(ActivityB.this, ActivityC.class));
+          executeTimeConsumingTask();
+          finish();
+        }
+        break;
+        default: {
+
+        }
+      }
     }
   };
 
