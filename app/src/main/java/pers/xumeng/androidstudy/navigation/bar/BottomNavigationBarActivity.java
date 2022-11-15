91181b8343e3fd4e4e50ce512cb99e78bb19ceac
XuMeng-0 <xumeng_135@qq.com>
2022-11-15 18:31:49 +0800

    feat(底部导航栏): 监听导航栏菜单项的点击事件，切换对应的 Fragment。禁用 ViewPager2 的滑动切换功能。

--- app/src/main/java/pers/xumeng/androidstudy/navigation/bar/BottomNavigationBarActivity.java
+++ app/src/main/java/pers/xumeng/androidstudy/navigation/bar/BottomNavigationBarActivity.java
@@ -1,10 +1,14 @@
 package pers.xumeng.androidstudy.navigation.bar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 import pers.xumeng.androidstudy.R;
 import pers.xumeng.androidstudy.databinding.NavigationActivityBottomNavigationBarBinding;
@@ -19,6 +23,32 @@ public class BottomNavigationBarActivity extends AppCompatActivity {
     int layoutId = R.layout.navigation_activity_bottom_navigation_bar;
     binding = DataBindingUtil.setContentView(this, layoutId);
     binding.mainFragmentContainer.setAdapter(new FragmentAdapter(this));
+    binding.mainFragmentContainer.setUserInputEnabled(false);
+    setNavigationItemSelectedListener();
+  }
+
+  private void setNavigationItemSelectedListener() {
+    binding.mainNavigationBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+      @Override
+      public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        String itemTitle = (String) item.getTitle();
+        switch (itemTitle) {
+          case "主页": {
+            binding.mainFragmentContainer.setCurrentItem(0);
+          }
+          break;
+          case "联系人": {
+            binding.mainFragmentContainer.setCurrentItem(1);
+          }
+          break;
+          case "我的": {
+            binding.mainFragmentContainer.setCurrentItem(2);
+          }
+          break;
+        }
+        return true;
+      }
+    });
   }
 
 }
