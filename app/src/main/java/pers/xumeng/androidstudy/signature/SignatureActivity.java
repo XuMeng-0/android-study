2d641cab8f0598db7faa42e51c95d701fb5786c3
XuMeng-0 <xumeng_135@qq.com>
2022-11-30 18:03:03 +0800

    feat(签名控件): 设置 Activity 的标题。

--- app/src/main/java/pers/xumeng/androidstudy/signature/SignatureActivity.java
+++ app/src/main/java/pers/xumeng/androidstudy/signature/SignatureActivity.java
@@ -3,6 +3,7 @@ package pers.xumeng.androidstudy.signature;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
@@ -17,6 +18,10 @@ public class SignatureActivity extends AppCompatActivity {
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    ActionBar actionBar = getSupportActionBar();
+    if (actionBar != null) {
+      actionBar.setTitle("签名控件的使用");
+    }
     binding = DataBindingUtil.setContentView(this, R.layout.signature_activity_signature);
   }
 
