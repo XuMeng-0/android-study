815353abf9a4ffbd134ed47026a676462812f1af
XuMeng-0 <xumeng_135@qq.com>
2022-12-07 19:13:10 +0800

    feat(其他应用打开文件): 添加保存和用其他应用打开按钮，设置按钮的样式和监听器。

--- app/src/main/java/pers/xumeng/androidstudy/open/file/another/app/OpenFileWithAnotherAppActivity.java
+++ app/src/main/java/pers/xumeng/androidstudy/open/file/another/app/OpenFileWithAnotherAppActivity.java
@@ -1,6 +1,7 @@
 package pers.xumeng.androidstudy.open.file.another.app;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
@@ -23,6 +24,20 @@ public class OpenFileWithAnotherAppActivity extends AppCompatActivity {
       actionBar.setTitle("其他应用打开文件");
     }
     binding = DataBindingUtil.setContentView(this, R.layout.another_activity_open_file_another_app);
+    binding.setListener(this);
+  }
+
+
+  public void save() {
+    Toast.makeText(this, "保存", Toast.LENGTH_SHORT).show();
+  }
+
+  public void openWithAnotherApp() {
+    Toast.makeText(this, "用其他应用打开", Toast.LENGTH_SHORT).show();
+  }
+
+  public void openWithAnotherAppChooseDefault() {
+    Toast.makeText(this, "用其他应用打开并选择默认应用", Toast.LENGTH_SHORT).show();
   }
 
 }
