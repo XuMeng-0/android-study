93afb441133ee3c673a26f064fc09af28d136b0d
XuMeng-0 <xumeng_135@qq.com>
2023-02-17 18:46:10 +0800

    feat(ipc-aidl): 创建运行在服务端进程中的 Service，实现 IBookManager.Stub 的方法，并创建实例，作为 onBind（）方法的返回值。

---
+++ app/src/main/java/pers/xumeng/androidstudy/ipc/aidl/BookManagerService.java
@@ -0,0 +1,44 @@
+package pers.xumeng.androidstudy.ipc.aidl;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.RemoteException;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class BookManagerService extends Service {
+
+  private static final String TAG = BookManagerService.class.getSimpleName();
+
+  private final CopyOnWriteArrayList<Book> bookList = new CopyOnWriteArrayList<>();
+  private final Binder binder = new IBookManager.Stub() {
+    @Override
+    public List<Book> getBookList() throws RemoteException {
+      return bookList;
+    }
+
+    @Override
+    public void addBook(Book book) throws RemoteException {
+      bookList.add(book);
+    }
+  };
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    bookList.add(new Book(1, "Android"));
+    bookList.add(new Book(2, "iOS"));
+  }
+
+  @Nullable
+  @Override
+  public IBinder onBind(Intent intent) {
+    return binder;
+  }
+
+}
